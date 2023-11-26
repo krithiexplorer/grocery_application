fd6f4b43b5e608260bce5399e2ea2ee8bdee8e54
krithiexplorer <kiruthiga.kameswaran@gmail.com>
2023-11-26 22:08:09 +0530

    image urls configuration

--- grocery_project/settings.py
+++ grocery_project/settings.py
@@ -126,8 +126,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 # Default primary key field type
